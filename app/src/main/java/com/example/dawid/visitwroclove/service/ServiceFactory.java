717b87b6f8c6c382240a67837f6391af92525d48
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2019-04-06 15:30:51 +0200

    HOTFIX: Add Stetho networking.

--- app/src/main/java/com/example/dawid/visitwroclove/service/ServiceFactory.java
+++ app/src/main/java/com/example/dawid/visitwroclove/service/ServiceFactory.java
@@ -1,8 +1,10 @@
 package com.example.dawid.visitwroclove.service;
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,6 +12,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ServiceFactory {
 
     public static <T> T createRetrofitService(final Class<T> clazz, final String endPoint) {
+
+        OkHttpClient okClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss")
                 .setLenient()
@@ -17,6 +24,7 @@ public class ServiceFactory {
 
 
         final Retrofit restAdapter = new Retrofit.Builder()
+                .client(okClient)
                 .baseUrl(endPoint)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
